a1de318df7598777532559b7a70e4cd997a4a4ae
Martin Nilsson <mune2000@msn.com>
2012-12-06 16:53:09 +0100

    Updaterat om_oss fil...

--- templates/om_oss.page.tpl.php
+++ templates/om_oss.page.tpl.php
@@ -1,1 +1,8 @@
-// class för om_oss
+<!-- Om oss sidan -->
+<?php
+ $type = 'page';
+?>
+<div id="content">
+	<h2><?php print($title); ?></h2>
+	<p><?php print($content); ?></p>
+</div><!-- end content -->
