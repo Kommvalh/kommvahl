7bf6558efe44823af337781ec55716b16cd9d95a
Mitsa Tajziehchi <mitsa23@yahoo.com>
2012-12-06 10:48:57 +0100

    html css

---
+++ html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<link href="css/layout.css" rel="stylesheet">
+	<title> </title>
+	</head>
+	<body>
+<div class="wrapper">
+
+<div class="header">
+</div>
+
+<div class="navbar"
+<menu><!--man kan använda <ul><li> också -->
+<a href="html.php">Home<a/>
+<a href="html.php?page=about">About<a/>
+<a href="html.php?page=products">Products<a/>
+<a href="html.php?page=contact">Contact<a/>
+</menu>
+</div>
+
+<div class="content">
+</div>
+
+<div class="footer">
+</div>
+
+</div> <!--end wrapper -->
+</body>
+</html>
