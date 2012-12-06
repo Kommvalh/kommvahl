cb6c150d6faecd62c96d76f8684a3d1b49f84e07
Kenth Söderholm <kenth.south@gmail.com>
2012-12-06 13:39:44 +0100

    smallfix

--- html_produktlista.php
+++ html_produktlista.php
@@ -23,9 +23,12 @@
 			</div><!-- end navbar -->
 			
 			<div id="content">
-				<h2>Testing!!!</h2>
-				<p>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h2>Produkter</h2>
+				<ul>
+					<li> <a href="#"> </a></li>
+					<li> <a href="#"> </a></li>
+					<li> <a href="#"> </a></li>
+				</ul>
 			</div><!-- end content -->
 			
 			<div id="footer">
